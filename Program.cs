03b6ac093a8e79f735d1df30f604316bfe74ef13
arnoroliveira <p.oliviras@gmail.com>
2024-10-20 22:17:03 -0300

    Update Projeto Celular Dio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+// Iphone i1 = new Iphone("837388", "Iphone 16 Pro Max", "GSHFJSK19379", 32);
+// i1.Ligar();
+// i1.InstalarAplicativo("Youtube");
+// i1.ReceberLigacao();
+
+Nokia n1 = new Nokia("7585749", "Nokia One", "HFJGH8340", 64);
+n1.Ligar();
+n1.ReceberLigacao();
+n1.InstalarAplicativo("WhatsApp");
